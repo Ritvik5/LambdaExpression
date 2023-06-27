51edee473308d055ca84404479436f02f2f813cd
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-27 23:57:28 +0530

    [Ritvik]Add: Welcome Message

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Lambda Expression!!!");
+        }
+    }
+}
